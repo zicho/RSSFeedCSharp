e9574532a0faa82a7b691ed10736105627a56354
Jakob <jakob.handfast@gmail.com>
2017-10-20 01:05:45 +0200

    Har lagt till metoder för att hämta data från en RSS feed och spara ner dem till en .xml fil i projektmappen. (Metoderna ligger just nu i main med allt annat)

--- CSharpProject/Views/Main.xaml.cs
+++ CSharpProject/Views/Main.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text;
 using System.Windows;
+using System.IO;
 
 namespace CSharpProject.Views
 {
@@ -36,6 +37,57 @@ namespace CSharpProject.Views
         }
 
         private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String xmlText = DownloadString(RSSTextBox.Text);
+            if (xmlText != null)
+            {
+                String RSSName = RSSNameTextBox.Text;
+                if (RSSName != null)
+                {
+                    CreateXMLFile(xmlText,RSSName);
+                }
+                
+            }
+        }
+
+        public void CreateXMLFile(String content, String name)
+        {
+            String path = (Environment.CurrentDirectory +"\\XML-folder"); //Path to a folder containing all XML files in the project directory
+            if (Directory.Exists(path) == false)
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            path = Path.Combine(Environment.CurrentDirectory, @"XML-folder\", name + ".xml");
+            if (!File.Exists(path)) //if there is no file with such name we go ahead and create it
+            {
+                File.AppendAllText(path, content);
+            }
+            else
+            {
+                MessageBox.Show("A podcast with that name already exist");
+            }
+            
+
+            
+        }
+
+        public string DownloadString(string url) //method to get all the data from an RSS feed
+        {
+            string text;
+            using (var client = new System.Net.WebClient())
+            {
+                text = client.DownloadString(url);
+            }
+            return text;
+        }
+
+            private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
 
         }
