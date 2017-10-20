64d9d6f32c365f96e2b3eaf9b407eb40f60927b7
Schmoege <christoffer.isacsson@gmail.com>
2017-10-20 13:13:34 +0200

    Sketch till uppdateringskontroll

--- Logic/Channel.cs
+++ Logic/Channel.cs
@@ -14,17 +14,34 @@ namespace Logic
         public String title { get; set; }
 
         DateTime lastUpdated { get; set; }
-        //int updateInterval;
+        int updateInterval;
         List<Podcast> podcastList;
 
-        public Channel(String title)
+        public Channel(String title, int updateInterval)
         {
             this.title = title;
             lastUpdated = DateTime.Now;
+            podcastList = new List<Podcast>;
+            this.updateInterval = updateInterval;
         }
+
+        public void ShallChannelBeUpdated()
+        {
+            DateTime updateDate = lastUpdated.AddDays(updateInterval);
+            
+            if(DateTime.Now.Equals(updateDate)) {
+                Podcast.FillPodcastList();
+            }
+            lastUpdated = DateTime.Now.AddDays(updateInterval); //bara sketch, vet att detta nya datum inte kommer sparas vid avstängning
+        }
+
         public Channel()
         {
 
+        }
+        public void AddPodcast(Podcast episode)
+        {
+            podcastList.Add(episode);
         }
     }
 }
