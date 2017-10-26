30a530cd4d904babf49366334fca9f667e86cd26
Martin Ström <zicho.calypso@gmail.com>
2017-10-26 13:22:56 +0200

    Kommit en bit på edit

--- CSharpProject/Views/EditPodcast.xaml.cs
+++ CSharpProject/Views/EditPodcast.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Xml;
 using System.Xml.Linq;
+using static Logic.Exceptions.ValidationException;
 
 namespace CSharpProject.Views
 {
@@ -23,7 +24,7 @@ namespace CSharpProject.Views
     /// </summary>
     /// 
 
-    
+
 
     public partial class EditPodcast : Window
     {
@@ -37,11 +38,15 @@ namespace CSharpProject.Views
         private List<Category> categoryList = Category.CategoryList;
         public List<Category> CategoryList { get => categoryList; set => categoryList = value; }
 
-        
+        public Logic.Exceptions.ValidationException.ValidatorList validator = new ValidatorList();
+        public Logic.Exceptions.ValidationException.BoxValidator boxValidator = new BoxValidator();
 
         public EditPodcast(MainWindow main)
         {
             InitializeComponent();
+
+            validator.Add(new Validator());
+            validator.Add(new LengthValidator(3));
 
             if (FeedList.Count() > 0)
             {
@@ -66,11 +71,11 @@ namespace CSharpProject.Views
             {
                 LoadInfo();
             }
-            catch(IndexOutOfRangeException)
+            catch (IndexOutOfRangeException)
             {
 
             }
-            
+
 
             Closing += (s, e) => main.IsEnabled = true;
             Closing += (s, e) => main.InitializeComboBoxes(); //refreshes the category combobox to display new category
@@ -85,48 +90,53 @@ namespace CSharpProject.Views
         {
             var item = feedComboBox.SelectedIndex;
 
-            if(FeedList.Count > 0) {
-                
+            if (FeedList.Count > 0)
+            {
+
                 feedComboBox.IsEnabled = true;
                 nameTextBox.IsEnabled = true;
                 URLTextBox.IsEnabled = true;
                 intervalComboBox.IsEnabled = true;
                 categoryComboBox.IsEnabled = true;
 
+                buttonSave.IsEnabled = true;
+                buttonDelete.IsEnabled = true;
+
                 var Name = FeedList[item].Name;
-            var URL = FeedList[item].URL;
-            var Interval = FeedList[item].UpdateInterval;
-            var Category = FeedList[item].Category;
+                var URL = FeedList[item].URL;
+                var Interval = FeedList[item].UpdateInterval;
+                var Category = FeedList[item].Category;
 
-            nameTextBox.Text = Name;
-            URLTextBox.Text = URL;
+                nameTextBox.Text = Name;
+                URLTextBox.Text = URL;
 
-            if(Interval == 1)
-            {
-                intervalComboBox.SelectedIndex = 0;
-            }
-
-            if (Interval == 3)
-            {
-                intervalComboBox.SelectedIndex = 1;
-            }
-
-            if (Interval == 7)
-            {
-                intervalComboBox.SelectedIndex = 2;
-            }
-
-            for (int i = 0; i < categoryComboBox.Items.Count; i++)
-            {
-                string value = categoryComboBox.Items[i].ToString();
+                if (Interval == 1)
                 {
-                    if(value == Category)
+                    intervalComboBox.SelectedIndex = 0;
+                }
+
+                if (Interval == 3)
+                {
+                    intervalComboBox.SelectedIndex = 1;
+                }
+
+                if (Interval == 7)
+                {
+                    intervalComboBox.SelectedIndex = 2;
+                }
+
+                for (int i = 0; i < categoryComboBox.Items.Count; i++)
+                {
+                    string value = categoryComboBox.Items[i].ToString();
                     {
-                        categoryComboBox.SelectedIndex = i;
+                        if (value == Category)
+                        {
+                            categoryComboBox.SelectedIndex = i;
+                        }
                     }
                 }
             }
-            } else
+            else
             {
                 feedComboBox.Items.Add("No feeds here yet.");
                 feedComboBox.SelectedIndex = 0;
@@ -136,6 +146,9 @@ namespace CSharpProject.Views
                 URLTextBox.IsEnabled = false;
                 intervalComboBox.IsEnabled = false;
                 categoryComboBox.IsEnabled = false;
+
+                buttonSave.IsEnabled = false;
+                buttonDelete.IsEnabled = false;
             }
         }
 
@@ -154,8 +167,6 @@ namespace CSharpProject.Views
         private void Window_Closed(object sender, EventArgs e)
         {
             this.Hide();
-            ((MainWindow)this.Owner).IsEnabled = true;
-            ((MainWindow)this.Owner).Topmost = true;
         }
 
         private void Window_Closing(object sender, EventArgs e)
@@ -166,7 +177,7 @@ namespace CSharpProject.Views
 
         private void buttonDelete_Click(object sender, RoutedEventArgs e)
         {
-            Name = nameTextBox.Text;
+            Name = feedComboBox.Text;
 
             MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show($"Do you really wish to delete podcast {Name}?", $"Confirm deletion of {Name}", System.Windows.MessageBoxButton.YesNo);
             if (messageBoxResult == MessageBoxResult.Yes)
@@ -177,33 +188,75 @@ namespace CSharpProject.Views
 
                 XElement settings = XElement.Load(Environment.CurrentDirectory + @"\settings.xml");
                 var query = from element in settings.Descendants()
-                          where (string)element.Element("Id") == Id.ToString()
-                          select element;
+                            where (string)element.Element("Id") == Id.ToString()
+                            select element;
                 if (query.Count() > 0)
                     query.First().Remove();
                 settings.Save(Environment.CurrentDirectory + @"\settings.xml");
 
                 FeedList.RemoveAt(item); // Ta bort ur feedlist också
+
+                System.IO.DirectoryInfo directory = new DirectoryInfo(Environment.CurrentDirectory + $@"\podcasts\{Name}");
+
+                directory.Delete(true);
+
+                MessageBox.Show($"Feed {Name} was deleted.");
+                ((MainWindow)this.Owner).UpdateFeedList();
+                LoadInfo();
+
             }
-
-
-            System.IO.DirectoryInfo directory = new DirectoryInfo(Environment.CurrentDirectory + $@"\podcasts\{Name}");
-
-            directory.Delete(true);
-
-            MessageBox.Show($"Feed {Name} was deleted.");
-            ((MainWindow)this.Owner).UpdateFeedList();
-            LoadInfo();
         }
 
         private void buttonSave_Click(object sender, RoutedEventArgs e)
         {
+            Name = feedComboBox.Text;
+            try
+            {
+                validator.Validate(URLTextBox.Text, "RSS URL", true); // PASSING A BOOLEAN INTO THIS METHOD MEANS IT DOES AN URL VALIDATION USING AN OVERLOAD ON THE VALIDATOR CLASS
+                validator.Validate(nameTextBox.Text, "Name");
+                boxValidator.Validate(categoryComboBox.SelectedIndex, "category");
+                boxValidator.Validate(intervalComboBox.SelectedIndex, "download interval");
 
+                MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show($"Save changes to {Name}?", $"Confirm edit of {Name}", System.Windows.MessageBoxButton.YesNo);
+                if (messageBoxResult == MessageBoxResult.Yes)
+                {
+
+                    var item = feedComboBox.SelectedIndex;
+                    var Id = FeedList[item].Id;
+
+                    var Name = nameTextBox.Text;
+                    var URL = URLTextBox.Text;
+                    var Category = categoryComboBox.SelectedValue;
+
+                    var Interval = 0;
+
+                    if (intervalComboBox.SelectedIndex == 0)
+                    {
+                        Interval = 1;
+                    }
+
+                    if (intervalComboBox.SelectedIndex == 1)
+                    {
+                        Interval = 3;
+                    }
+
+                    if (intervalComboBox.SelectedIndex == 2)
+                    {
+                        Interval = 7;
+                    }
+
+                    MessageBox.Show(Name + URL + Category + Interval);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Cannot save.");
+            }
         }
 
-        private void nameTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
+    private void nameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
 
-        }
     }
+}
 }
